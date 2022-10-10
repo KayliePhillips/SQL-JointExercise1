4af5119a4be607ddd7c48430a520cd4b7c06ea59
Kaylie Phillips <kaylie.travel@gmail.com>
2022-10-10 11:30:23 -0500

    updates after John reviewed in class

--- SQL-JoinsExercise1.sql
+++ SQL-JoinsExercise1.sql
@@ -28,6 +28,14 @@ ON e.EmployeeID = s.EmployeeID
 GROUP BY e.EmployeeID
 ORDER BY SUM(s.Quantity) DESC;
 
+-- How John did it during the review.  
+SELECT e.EmployeeID, e.FirstName, e.LastName, SUM(s.Quantity) AS Total_Quantity_Sold
+FROM bestbuy.employees e
+INNER JOIN bestbuy.sales s
+ON e.EmployeeID = s.EmployeeID
+GROUP BY s.EmployeeID
+ORDER BY Total_Quantity_Sold DESC;
+
 
 /* joins: find the name of the department, and the name of the category for Appliances and Games */
 SELECT c.name AS Category_Name, d.name AS Department_Name
@@ -39,7 +47,7 @@ WHERE c.Name = 'Appliances' OR c.Name ='Games';
 
 /* joins: find the product name, total # sold, and total price sold,
  for Eagles: Hotel California --You may need to use SUM() */
-SELECT p.name AS Product_Name, sum(s.quantity) AS Total_Sold, s.priceperunit * SUM(s.Quantity) AS Total_Price_Sold
+SELECT p.name AS Product_Name, SUM(s.Quantity) AS Total_Sold, SUM(s.Quantity*S.PricePerUnit) AS Total_Price_Sold
 FROM bestbuy.products p
 INNER JOIN bestbuy.Sales s 
 ON p.ProductID = s.ProductID
@@ -48,8 +56,8 @@ WHERE p.ProductID = '97';
 
 /* joins: find Product name, reviewer name, rating, and comment on the Visio TV. (only return for the lowest rating!) */
 SELECT p.name AS Product_Name, r.reviewer AS Reviewer_Name, r.rating AS Rating, r.comment
-FROM bestbuy.reviews r
-INNER JOIN bestbuy.products p
+FROM bestbuy.products p
+INNER JOIN bestbuy.reviews r
 ON p.ProductID = r.ProductID
 WHERE p.name = 'Visio TV' 
 ORDER BY r.rating
@@ -63,12 +71,12 @@ This query should return:
 -  the employee's first and last name
 -  the name of each product
 -  and how many of that product they sold */
-SELECT e.EmployeeID, e.FirstName, e.LastName, p.Name, SUM(s.quantity) AS Total_Sold
+SELECT e.EmployeeID, e.FirstName, e.LastName, p.Name AS Product_Name, SUM(s.Quantity) AS Quanity_Sold
 FROM bestbuy.sales s
 INNER JOIN bestbuy.employees e
 ON s.EmployeeID AND e.EmployeeID
 INNER JOIN bestbuy.products p
 ON s.ProductID AND p.ProductID
 GROUP BY e.EmployeeID, p.Name
-ORDER BY e.EmployeeID
+ORDER BY e.EmployeeID;
 
